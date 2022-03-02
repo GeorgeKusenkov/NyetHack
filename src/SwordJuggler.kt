28756aa534ed45bd41c40908d7c8ebf1670f225b
g-jaz <g-jazzman@yandex.ru>
2022-03-02 21:28:00 +0500

    l1.1

---
+++ src/SwordJuggler.kt
@@ -0,0 +1,22 @@
+class SwordJuggler {
+}
+
+fun main() {
+    var swordsJuggling: Int? = null
+    val isJugglingProficient = (1..3).shuffled().last() == 3
+    if(isJugglingProficient) {
+        swordsJuggling = 2
+    }
+
+    proficiencyCheck(swordsJuggling)
+    swordsJuggling = swordsJuggling!!.plus(1)
+
+    println("You juggle $swordsJuggling swords!")
+}
+
+fun proficiencyCheck (swordJuggling: Int?) {
+    swordJuggling ?: throw UnskilledSwordJugglerException()
+//    swordJuggling ?: throw IllegalStateException("Player cannot juggle swords")
+}
+
+class UnskilledSwordJugglerException() : IllegalStateException("Player cannot juggle swords")
